323d53d818473b8d2c20c77d87a451553f79de78
sadmanahmed <abirsadmanahmed177@gmail.com>
2020-08-04 06:11:13 +0600

    Validtations done

--- app/admin/schools.rb
+++ app/admin/schools.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register School do
-
+  permit_params :name
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register School do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
--- app/admin/students.rb
+++ app/admin/students.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Student do
-
+  permit_params :name,:student_number
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register Student do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
