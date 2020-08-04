1ea04484208a863d10b3e6257d8b2aa050f2af82
sadmanahmed <abirsadmanahmed177@gmail.com>
2020-08-05 04:12:45 +0600

    Added a image column in students table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_001944) do
+ActiveRecord::Schema.define(version: 2020_08_04_221028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_08_04_001944) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "school_id"
+    t.string "image"
     t.index ["school_id"], name: "index_students_on_school_id"
   end
 
